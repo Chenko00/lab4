181368735d32eeabee44cbfe94363a64940c0e67
plitchenko_a <plitchenko_a@400D-58>
2021-10-12 15:23:55 +0700

    Работа над event

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -2,11 +2,51 @@
 
 namespace lab4
 {
+    class Account
+    {
+        public delegate void AccountHandler(string message);
+        public event AccountHandler Notify;              
+        public Account(int sum)
+        {
+            Sum = sum;
+        }
+        public int Sum { get; private set; }
+        public void Put(int sum)
+        {
+            Sum += sum;
+            Notify?.Invoke($"На счет поступило: {sum}");  
+        }
+        public void Take(int sum)
+        {
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                Notify?.Invoke($"Со счета снято: {sum}");   
+            }
+            else
+            {
+                Notify?.Invoke($"Недостаточно денег на счете. Текущий баланс: {Sum}"); ;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account acc = new Account(100);
+            acc.Notify += DisplayMessage;   
+            acc.Put(20);   
+            Console.WriteLine($"Сумма на счете: {acc.Sum}");
+            acc.Take(70);  
+            Console.WriteLine($"Сумма на счете: {acc.Sum}");
+            acc.Take(180);
+            Console.WriteLine($"Сумма на счете: {acc.Sum}");
+            Console.Read();
+        }
+        private static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
+
